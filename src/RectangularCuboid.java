913ce636fb275d97cc1660daba65ce9a6b197847
BT_1N3_05 <@bths.edu>
2024-05-21 11:02:53 -0400

    i did too much refactoring and MORE Edges!!!!

--- src/RectangularCuboid.java
+++ src/RectangularCuboid.java
@@ -27,22 +27,12 @@ public class RectangularCuboid extends Polyhedron {
         setVertices(new Vertex[]{a, b, c, d, e, f, g, h});
         center = new Vertex((g.getX() + a.getX()) / 2, (g.getY() + a.getY()) / 2, (g.getZ() + a.getZ()) / 2);
         setCenter(center);
-        setEdges(new Edge[]{new Edge(a, b), new Edge(b, c), new Edge(c, d), new Edge(d, a), new Edge(e, f), new Edge(f, g), new Edge(g, h), new Edge(h, e)});
+        setEdges(new Edge[]{new Edge(a, b), new Edge(b, c), new Edge(c, d), new Edge(d, a), new Edge(e, f), new Edge(f, g), new Edge(g, h), new Edge(h, e), new Edge(a, e), new Edge(b, f), new Edge(c, g), new Edge(d, h)});
     }
 
     @Override
     public void draw(Graphics2D g2D) {
         super.draw(g2D);
-        g2D.drawLine(a.getIntX(), a.getIntY(), f.getIntX(), f.getIntY());
-        g2D.drawLine(b.getIntX(), b.getIntY(), e.getIntX(), e.getIntY());
-        g2D.drawLine(c.getIntX(), c.getIntY(), g.getIntX(), g.getIntY());
-        g2D.drawLine(d.getIntX(), d.getIntY(), g.getIntX(), g.getIntY());
-        g2D.drawLine(a.getIntX(), a.getIntY(), d.getIntX(), d.getIntY());
-        g2D.drawLine(e.getIntX(), e.getIntY(), g.getIntX(), g.getIntY());
-
-        g2D.drawLine(b.getIntX(), b.getIntY(), g.getIntX(), g.getIntY());
-        g2D.drawLine(c.getIntX(), c.getIntY(), f.getIntX(), f.getIntY());
-        g2D.drawOval(center.getIntX() - 5, center.getIntY() - 5, 10, 10);
     }
 
     public Vertex getCenter() {
